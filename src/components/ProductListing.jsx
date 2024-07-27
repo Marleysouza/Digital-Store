a7fa57566319163f08ce2caa7ba39170c53a131e
Marley Souza <ctt.marley@gmail.com>
2024-07-27 20:58:43 -0300

    :recycle: refactor: Adição de propiedades em productlisting para receber arrays.

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -1,12 +1,11 @@
-import { Produtos } from "../data/produtos"
 import ProductCard from "./ProductCad"
 
-const ProductListing = ({ qtd }) => {
+const ProductListing = ({ Produtos, qtd }) => {
 
     let Product = Produtos.slice(0, qtd);
 
     return (
-        <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between",paddingBlock:"40px" }}>
+        <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between", paddingBlock: "40px" }}>
             {Product.map((pdt, index) => (
                 <div key={index}>
                     <ProductCard image={pdt.image} name={pdt.name} price={pdt.price} priceDiscount={pdt.priceDiscount} />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import ProductListing from "../components/ProductListing"
 import Section from "../components/Section"
 import Layout from "./Layout"
+import { Produtos } from "../data/produtos"
 
 const HomePage = () => {
     return (
         <Layout>
             <Section tilte={"Produtos em alta"} titleAling={"left"} link={"#"}>
-                <ProductListing qtd={8}/>
+                <ProductListing Produtos={Produtos} qtd={8} />
             </Section>
         </Layout>
     )
