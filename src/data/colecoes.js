2eb7168c4f0086910a9e1aa7e52db642c461ff57
Marley Souza <ctt.marley@gmail.com>
2024-07-28 07:19:05 -0300

    :sparkles: feat: HomePage

---
+++ src/data/colecoes.js
@@ -0,0 +1,5 @@
+export const colecoes = [
+    { src: "https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/main/public/collection-1.png" },
+    { src: "https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/main/public/collection-2.png" },
+    { src: "https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/main/public/collection-3.png" }
+]
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,24 @@
 import ProductListing from "../components/ProductListing"
 import Section from "../components/Section"
 import Layout from "./Layout"
+import Gallery from "../components/Gallery"
 import { Produtos } from "../data/produtos.js"
 import { slide } from "../data/slide.js"
-import Gallery from "../components/Gallery"
+import { colecoes } from "../data/colecoes.js"
 
 const HomePage = () => {
     return (
         <Layout>
-            <Gallery slide={slide} />
+            <main>
+                <Gallery slide={slide} />
+            </main>
+            <Section tilte={"Coleções em destaque"} titleAling={"center"} >
+                <div style={{ display: "flex", justifyContent: "space-around", paddingBlock: "40px", gap: "20px" }}>
+                    <a href="#"><img style={{ width: "100%", borderRadius: "8px" }} src={colecoes[0].src} /></a>
+                    <a href="#"><img style={{ width: "100%", borderRadius: "8px" }} src={colecoes[1].src} /></a>
+                    <a href="#"><img style={{ width: "100%", borderRadius: "8px" }} src={colecoes[2].src} /></a>
+                </div>
+            </Section>
             <Section tilte={"Produtos em alta"} titleAling={"left"} link={"#"}>
                 <ProductListing Produtos={Produtos} qtd={8} />
             </Section>
