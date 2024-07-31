373433ce9ecb6e0f6e83b1f4dad8d5ee675c6505
Marley Souza <ctt.marley@gmail.com>
2024-07-30 23:01:17 -0300

    :sparkles: feat:funcionalidade de clique adicionada em Gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import left from '../assents/svg/arrow-left.svg'
 import right from '../assents/svg/arrow-right.svg'
+import { useState } from 'react';
 
 const Gallery = ({ slide, showThumbs, width, height, text, link }) => {
+
+    const [indiceAtual, setIndiceAtual] = useState(0);
+
+    const proximaImagem = () => {
+        setIndiceAtual((indiceAtual + 1) % slide.length);
+    };
+
+    const imagemAnterior = () => {
+        setIndiceAtual((indiceAtual - 1 + slide.length) % slide.length);
+    };
+
     return (
         <div id="gallery" style={{ width: width }}>
             <div id='slides-content'>
                 <div id="slides">
                     {slide.map((image, index) => (
-                        <div key={index} className="slide" >
+                        <div key={index} className={`slide ${index === indiceAtual ? 'active' : ''}`} >
                             <img src={image.image} style={{ height: height }} />
                             {text && (
                                 <div>
@@ -19,13 +31,13 @@ const Gallery = ({ slide, showThumbs, width, height, text, link }) => {
                         </div>
                     ))}
                 </div>
-                <button id='prev'><img src={left} /></button>
-                <button id='next'><img src={right} /></button>
+                <button id='prev' onClick={imagemAnterior}><img src={left} /></button>
+                <button id='next' onClick={proximaImagem}><img src={right} /></button>
             </div>
             {showThumbs && (
                 <div id="thumbnails">
                     {slide.map((image, index) => (
-                        <div key={index} className="thumbnail">
+                        <div key={index} className={`thumbnail ${index === indiceAtual ? 'active' : ''}`} onClick={() => setIndiceAtual(index)}>
                             <img src={image.image} />
                         </div>
                     ))}
